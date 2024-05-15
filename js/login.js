3baa5223fd4973191e580a608acc3d8227e9c8d7
Mai Anh Bắc <maianhbac17052001@gmail.com>
2024-05-15 15:05:30 +0700

    update

---
+++ js/login.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnSubmit = document.getElementById('sign-in')
+    btnSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        const getByLogin = async (email, password) => {
+            const res = await fetch("https://api.escuelajs.co/api/v1/auth/login", {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            })
+            const data = await res.json()
+            if (data.access_token) {
+                authorization(data.access_token);
+            }
+            return
+    }
+        getByLogin(email, password)
+        const authorization = async (token) => {
+        const res = await fetch('https://api.escuelajs.co/api/v1/auth/profile', {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            }
+        })
+        const accessToken = await res.json();
+        localStorage.setItem('user', JSON.stringify(accessToken))
+        window.location.href = '/index.html'
+    }
+
+
+})
+})
+document.addEventListener('DOMContentLoaded', () => {
+    const welcomeElement = document.getElementById('welcome');
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.name) {
+        welcomeElement.innerHTML = user.name;
+    } else {
+        return
+    }
+});
